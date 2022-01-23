4d3ce13f6d1b1f2a83ce88f7562876baa3c2d3e9
kkoops <kthr242468@icloud.com>
2022-01-24 02:59:31 +0900

    day1終了まで

---
+++ day_1/prob1/prob1-6.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main(){
+    string a,b;
+    cout<<"姓を入力:";
+    cin>>a;
+    cout<<"名を入力:";
+    cin>>b;
+    cout << "名前は「"<<a<<b<<"」です。"<< endl;
+    return 0;
+}
