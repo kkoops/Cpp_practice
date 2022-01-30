3fb2d6f75f536ce6bee86512b3c3b2d2f5120d5b
kkoops <kthr242468@icloud.com>
2022-01-30 18:19:11 +0900

    G++

--- day_6/list6-1/ambulance.cpp
+++ day_6/list6-1/ambulance.cpp
@@ -3,16 +3,12 @@
 
 using namespace std;
 
-CAmbulance::CAmbulance():m_number(119)
-{
-    cout<<"CAmbulanceオブジェクト生成"<<endl;
+CAmbulance::CAmbulance() : m_number(119) {
+  cout << "CAmbulanceオブジェクト生成" << endl;
 }
 
-CAmbulance::~CAmbulance()
-{
-    cout<<"CAmbulanceオブジェクト破棄"<<endl;
-}
+CAmbulance::~CAmbulance() { cout << "CAmbulanceオブジェクト破棄" << endl; }
 
-void CAmbulance::savePeople(){
-    cout<<"救急救命活動"<<endl<<"呼び出しは"<<m_number<<"ばん"<<endl;
+void CAmbulance::savePeople() {
+  cout << "救急救命活動" << endl << "呼び出しは" << m_number << "ばん" << endl;
 }
--- day_6/list6-2/Vector2D.cpp
+++ day_6/list6-2/Vector2D.cpp
@@ -1,21 +1,15 @@
 #include "Vector2D.h"
 
-Vector2D::Vector2D()
-{
-    init();
-}
+Vector2D::Vector2D() { init(); }
 
-void Vector2D::setValue(int x,int y){
-    m_x=x;
-    m_y=y;
+void Vector2D::setValue(int x, int y) {
+  m_x = x;
+  m_y = y;
 }
-int Vector2D::getX(){
-    return m_x;
-}
-int Vector2D::getY(){
-    return m_y;
-}
+int Vector2D::getX() { return m_x; }
+int Vector2D::getY() { return m_y; }
 
-void Vector2D::init(){
-    m_x=0;m_y=0;
+void Vector2D::init() {
+  m_x = 0;
+  m_y = 0;
 }
